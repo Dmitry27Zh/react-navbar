a7fb02f5c192065b6a898fc3965e4bf1a9ac1810
Dmitry <dmitry27zh@gmail.com>
2024-01-06 15:54:34 +0500

    Apply routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './Navbar'
 import About from './pages/About'
 import Home from './pages/Home'
 import Pricing from './pages/Pricing'
 
 function App() {
-  let Component
-
-  switch (window.location.pathname) {
-    case '/':
-      Component = Home
-      break
-    case '/pricing':
-      Component = Pricing
-      break
-    case '/about':
-      Component = About
-      break
-    default:
-      Component = <h1>Unknown</h1>
-  }
-
   return (
     <>
       <Navbar />
       <div className="container">
-        <Component />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
     </>
   )
